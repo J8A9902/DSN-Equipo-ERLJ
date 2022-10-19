ded98c43dd58e761c3e1559de9545ad166a73c93
J8A9902 <j.ochoao@uniandes.edu.co>
2022-10-18 22:09:42 -0500

    sE CAMBIA NOMBRE DE APLICACIÓN DE CELERY

--- tasks/celery_tasks/flask_celery.py
+++ tasks/celery_tasks/flask_celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 from config import CELERY_BROKER
 
 def make_celery(app):
-    celery=Celery(__name__, broker=CELERY_BROKER)
+    celery=Celery(__name__,backend = CELERY_BROKER, broker=CELERY_BROKER)
     celery.conf.update(app.config)
     class ContextTask(celery.Task):
         def __call__(self, *args, **kwargs):
--- tasks/celery_tasks/tasks.py
+++ tasks/celery_tasks/tasks.py
@@ -3,7 +3,7 @@ from celery import Celery
 from config import CELERY_BROKER
 from werkzeug.utils import secure_filename
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 from config import UPLOAD_FOLDER
 from models import Task
 from database import db
@@ -13,41 +13,20 @@ from app import app
 
 celery_app = Celery(__name__, broker=CELERY_BROKER)
 
-@celery_app.task()
-def create_file(uploaded_file, task_id, user_id):
-    if(uploaded_file and uploaded_file.filename):
-        try:
-            file_name = secure_filename(uploaded_file.filename)
-            file_path = os.path.join(f'{UPLOAD_FOLDER}/{user_id}', file_name)
-
-            if(not os.path.exists(file_path)):
-                os.makedirs(file_path)
-
-            
-            uploaded_file.save(os.path.join(UPLOAD_FOLDER, file_name))
-
-            task = Task.get_by_id(task_id)
-
-            task.status = TaskStatus.UPLOADED.value
-            task.update()
-
-        except Exception as e:
-            raise Exception('Error uploading the file, please try again')
-    else:
-        raise Exception('File not provided')
-        
+   
 celery_app= make_celery(app.py)
-celery_app.conf.beat_schedule = {
-    'add-every-1-seconds': {
+
+app.config['CELERYBEAT_SCHEDULE'] = {
+    # Executes every minute
+    'periodic_task-every-minute': {
         'task': 'celery_tasks.tasks.convertir_archivos',
-        'schedule': 1.0,
-        'args': ('prueba', datetime.utcnow())
-    },
-}  #minute='*/1'  crontab(sec='*/1')
+        'schedule': timedelta(seconds=1)
+    }
+}
 celery_app.conf.timezone = 'UTC'
 
-@celery_app.task(name='celery_tasks.tasks.convertir_archivos', bind=True, ignore_result=False)
-def convertir_archivos(self):
+@celery_app.task()
+def convertir_archivos():
     print("convertir_archivos")
     tarea=Task.query.with_for_update().filter(Task.status=="UPLOADED").first()
     if tarea is None:
