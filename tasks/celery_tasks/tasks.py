64b3d8203db54db73984183995e67515a76ec9f0
Rafael Ortiz <rd.ortizr1@uniandes.edu.co>
2022-10-18 22:54:18 -0500

    Task

--- tasks/celery_tasks/tasks.py
+++ tasks/celery_tasks/tasks.py
@@ -9,28 +9,47 @@ from models import Task
 from database import db
 from helpers.tasks_status_enum import TaskStatus
 from .flask_celery import make_celery
-from app import app
+
 
 celery_app = Celery(__name__, broker=CELERY_BROKER)
 
-   
-celery_app= make_celery(app.py)
-
-app.config['CELERYBEAT_SCHEDULE'] = {
-    # Executes every minute
-    'periodic_task-every-minute': {
+celery_app.conf.beat_schedule = {
+    'add-every-1-seconds': {
         'task': 'celery_tasks.tasks.convertir_archivos',
-        'schedule': timedelta(seconds=1)
-    }
-}
+        'schedule': 1.0,
+        'args': (16, 16)
+    },
+}  #minute='*/1'  crontab(sec='*/1')
 celery_app.conf.timezone = 'UTC'
 
 @celery_app.task()
-def convertir_archivos():
+def create_file(uploaded_file, task_id, user_id):
+    if(uploaded_file and uploaded_file.filename):
+        try:
+            file_name = secure_filename(uploaded_file.filename)
+            file_path = os.path.join(f'{UPLOAD_FOLDER}/{user_id}', file_name)
+
+            if(not os.path.exists(file_path)):
+                os.makedirs(file_path)
+
+            
+            uploaded_file.save(os.path.join(UPLOAD_FOLDER, file_name))
+
+            task = Task.get_by_id(task_id)
+
+            task.status = TaskStatus.UPLOADED.value
+            task.update()
+
+        except Exception as e:
+            raise Exception('Error uploading the file, please try again')
+    else:
+        raise Exception('File not provided')
+
+
+@celery_app.task(name='celery_tasks.tasks.convertir_archivos', bind=True, ignore_result=False)
+def convertir_archivos(*args):
     print("convertir_archivos")
     tarea=Task.query.with_for_update().filter(Task.status=="UPLOADED").first()
-    if tarea is None:
-        return 'Error procesando Conversión', 409
 
     try:
         nameTask = tarea.file_name.split('.')[0]
@@ -38,6 +57,6 @@ def convertir_archivos():
         ##shutil.copy(os.getcwd()+'/archivos/input/'+nombre, os.getcwd()+'/archivos/output/'+nombre)
         tarea.status="PROCESSED"
         db.session.commit()
-    except IntegrityError:
+    except Exception as e:
         db.session.rollback()
-        return 'Error procesando Conversión', 409
+        return f'Error procesando Conversión: {e}', 409
