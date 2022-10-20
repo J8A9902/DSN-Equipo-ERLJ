692cf60e21c6d47063c2468125d91d6832f08a7e
Rafael Ortiz <rd.ortizr1@uniandes.edu.co>
2022-10-19 21:35:09 -0500

    scheduler

--- tasks/app.py
+++ tasks/app.py
@@ -2,10 +2,47 @@ from flask import Flask
 
 from helpers.extensions import register_blueprints, initialize_database
 
+from celery import Celery
+from config import CELERY_BROKER
 
-app: Flask = Flask(__name__)
+
+app=Flask(__name__)
+  
 
 
-with app.app_context():
-    register_blueprints(app)
-    initialize_database(app)
+def make_celery(app):
+    celery=Celery(__name__,backend = CELERY_BROKER, broker=CELERY_BROKER)
+    celery.conf.update(app.config)
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+    celery.Task=ContextTask
+    return celery
+
+
+
+
+app_context=app.app_context()
+app_context.push()
+
+register_blueprints(app)
+initialize_database(app)
+
+celery_app=make_celery(app)
+celery_app.conf.beat_schedule = {
+    'add-every-30-seconds': {
+        'task': 'convertir_archivos',
+        'schedule': 1.0,
+        'args': ('prueba')
+    },
+}  #minute='*/1'  crontab(sec='*/1')
+celery_app.conf.timezone = 'UTC'
+
+@celery_app.task(name='convertir_archivos',bind=True)
+def convertir_archivos(*args):
+     print("convertir_archivos")
+
+
+
+
