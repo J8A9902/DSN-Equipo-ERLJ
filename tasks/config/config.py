e5889b6653380e4b7a233a8c101271d223886aeb
Rafael Ortiz <rd.ortizr1@uniandes.edu.co>
2022-10-28 21:03:22 -0500

    Update config.py

--- tasks/config/config.py
+++ tasks/config/config.py
@@ -2,5 +2,5 @@ import os
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 AUTH_SERVICE = os.environ.get('AUTH_SERVICE')
-UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
+UPLOAD_FOLDER = '/nfs/home'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
